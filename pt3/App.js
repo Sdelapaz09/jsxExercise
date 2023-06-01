8ceb324a209645acd1335a8e5737041029acf19c
Shenelle <119816184+Sdelapaz09@users.noreply.github.com>
2023-05-31 22:04:06 -0700

    App.js

---
+++ pt3/App.js
@@ -0,0 +1,21 @@
+function App(){
+  return (
+    <div>
+      <Person 
+        name="Shenelle"
+        age={30}
+        hobbies={["reading", "writing", "learning new things"])
+      />
+      <Person
+        name="Gloria"
+        age={32}
+        hobbies={["reading", "working out", "playing with the dog"]}
+      />
+      <Person
+        name="Liz"
+        age={36}
+        hobbies={["running", "baking", "cleaning"]}
+      />
+    </div>
+  );
+}
